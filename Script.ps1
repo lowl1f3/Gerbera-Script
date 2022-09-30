4bc6fc7e9f60187d7902aa8e01e16b57268628d7
lowl1f3 <muhamed2001bill2004@gmail.com>
2022-10-01 02:06:32 +0300

    Update Script.ps1

--- Script.ps1
+++ Script.ps1
@@ -406,11 +406,11 @@ if (Test-Path -Path "$env:ProgramFiles\Notepad++")
 
 	[xml]$config = Get-Content -Path "$env:APPDATA\Notepad++\config.xml" -Force
 	$config.Save("$env:APPDATA\Notepad++\config.xml")
-    
-    Start-Process -FilePath "$env:ProgramFiles\Notepad++\notepad++.exe" -Wait
+	
+	Start-Process -FilePath "$env:ProgramFiles\Notepad++\notepad++.exe" -Wait
 	# It is needed to use -wait to make Notepad++ apply written settings
 	Write-Warning -Message "Close Notepad++' window manually"
-    # Wait-Process -Name "notepad"
+	# Wait-Process -Name "notepad"
 	Start-Process -FilePath "$env:APPDATA\Notepad++\config.xml" -Wait
 
 	if (-not (Test-Path -Path $env:ProgramFiles\Notepad++\localization))
