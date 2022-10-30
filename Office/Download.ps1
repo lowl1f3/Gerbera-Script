adb2d16f7b848879fbf121b98d1f130b4b067e12
lowl1f3 <muhamed2001bill2004@gmail.com>
2022-10-31 00:42:04 +0200

    Updated Office installing

--- Office/Download.ps1
+++ Office/Download.ps1
@@ -1,28 +1,20 @@
 <#
 	.SYNOPSIS
 	Download Office 2019, 2021, and 365
-
 	.PARAMETER Branch
 	Choose Office branch: 2019, 2021, and 365
-
 	.PARAMETER Channel
 	Choose Office channel: 2019, 2021, and 365
-
 	.PARAMETER Components
 	Choose Office components: Access, OneDrive, Outlook, Word, Excel, PowerPoint, Teams
-
 	.EXAMPLE Download Office 2019 with the Word, Excel, PowerPoint components
 	DownloadOffice -Branch ProPlus2019Retail -Channel Current -Components Word, Excel, PowerPoint
-
 	.EXAMPLE Download Office 2021 with the Excel, Word components
-	DownloadOffice -Branch ProPlus2021Volume -Channel PerpetualVL2021 -Components Excel, Word, PowerPoint
-
+	DownloadOffice -Branch ProPlus2021Volume -Channel PerpetualVL2021 -Components Excel, Word
 	.EXAMPLE Download Office 365 with the Excel, Word, PowerPoint components
 	DownloadOffice -Branch O365ProPlusRetail -Channel SemiAnnual -Components Excel, OneDrive, Outlook, PowerPoint, Teams, Word
-
 	.LINK
 	https://config.office.com/deploymentsettings
-
 	.LINK
 	https://docs.microsoft.com/en-us/deployoffice/vlactivation/gvlks
 #>
@@ -37,7 +29,7 @@ function DownloadOffice
 		$Branch,
 
 		[Parameter(Mandatory = $true)]
-		[ValidateSet("Current", "PerpetualVL2021", "SemiAnnual")]
+		[ValidateSet("BetaChannel", "Current", "PerpetualVL2021", "SemiAnnual")]
 		[string]
 		$Channel,
 
@@ -51,20 +43,6 @@ function DownloadOffice
 	{
 		Write-Warning -Message "Default.xml doesn't exist"
 		exit
-	}
-
-
-	# Microsoft blocks Russian and Belarusian regions for Office downloading
-	# https://docs.microsoft.com/en-us/windows/win32/intl/table-of-geographical-locations
-	# https://en.wikipedia.org/wiki/2022_Russian_invasion_of_Ukraine
-	if (((Get-WinHomeLocation).GeoId -eq "203") -or ((Get-WinHomeLocation).GeoId -eq "29"))
-	{
-		# Set to Ukraine
-		$Script:Region = (Get-WinHomeLocation).GeoId
-		Set-WinHomeLocation -GeoId 241
-		Write-Warning -Message "Region changed to Ukrainian"
-
-		$Script:RegionChanged = $true
 	}
 
 	[xml]$Config = Get-Content -Path "$PSScriptRoot\Default.xml" -Encoding Default -Force
@@ -87,6 +65,10 @@ function DownloadOffice
 
 	switch ($Channel)
 	{
+		BetaChannel
+		{
+			($Config.Configuration.Add | Where-Object -FilterScript {$_.Channel -eq ""}).Channel = "BetaChannel"
+		}
 		Current
 		{
 			($Config.Configuration.Add | Where-Object -FilterScript {$_.Channel -eq ""}).Channel = "Current"
@@ -208,11 +190,6 @@ function DownloadOffice
 
 	$Config.Save("$PSScriptRoot\Config.xml")
 
-	# It is needed to remove these keys not to bypass Russian and Belarusian region blocks
-	Remove-Item -Path HKCU:\SOFTWARE\Microsoft\Office\16.0\Common\Experiment -Recurse -Force -ErrorAction Ignore
-	Remove-Item -Path HKCU:\SOFTWARE\Microsoft\Office\16.0\Common\ExperimentConfigs -Recurse -Force -ErrorAction Ignore
-	Remove-Item -Path HKCU:\SOFTWARE\Microsoft\Office\16.0\Common\ExperimentEcs -Recurse -Force -ErrorAction Ignore
-
 	# Download Office Deployment Tool
 	# https://www.microsoft.com/en-us/download/details.aspx?id=49117
 	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -253,13 +230,6 @@ function DownloadOffice
 
 # Download Offce. Firstly, download Office, then install it
 DownloadOffice -Branch O365ProPlusRetail -Channel SemiAnnual -Components Excel, Word, PowerPoint, Outlook, Teams, OneDrive
-
-if ($Script:RegionChanged)
-{
-	# Set to original region ID
-	Set-WinHomeLocation -GeoId $Script:Region
-	Write-Warning -Message "Region changed to original one"
-}
 
 # Install
 Start-Process -FilePath "$PSScriptRoot\setup.exe" -ArgumentList "/configure `"$PSScriptRoot\Config.xml`"" -Wait
