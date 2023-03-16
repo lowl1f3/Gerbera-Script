ff70489250b167c4c32d9de0aa4ca9e204d267ab
Eugene <46003912+lowl1f3@users.noreply.github.com>
2023-03-16 19:04:24 +0200

    Update Script.ps1

--- Scripts/Script.ps1
+++ Scripts/Script.ps1
@@ -1,11 +1,11 @@
-Write-Verbose -Message "Script_`"${{ github.ref_name }}`".zip" -Verbose
+Write-Verbose -Message "Script_`"`${{ github.ref_name }}`".zip" -Verbose
 
-New-Item -Path "Script_`"${{ github.ref_name }}`"" -ItemType Directory -Force
+New-Item -Path "Script_`"`${{ github.ref_name }}`"" -ItemType Directory -Force
 
-Get-ChildItem -Path src -Force | Copy-Item -Destination "Script_`"${{ github.ref_name }}`"" -Recurse -Force
+Get-ChildItem -Path src -Force | Copy-Item -Destination "Script_`"`${{ github.ref_name }}`"" -Recurse -Force
 $Parameters = @{
-    Path             = "Script_`"${{ github.ref_name }}`""
-    DestinationPath  = "Script_`"${{ github.ref_name }}`".zip"
+    Path             = "Script_`"`${{ github.ref_name }}`""
+    DestinationPath  = "Script_`"`${{ github.ref_name }}`".zip"
     CompressionLevel = "Fastest"
     Force            = $true
 }
